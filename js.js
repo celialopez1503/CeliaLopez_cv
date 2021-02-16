56bb19e4e1f1888a909ec4f3bc2aa453ef9e7f9a
celialopez1503 <celialopez1503@gmail.com>
2021-02-16 11:24:33 +0100

    mvnfvk

---
+++ js.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const hablar = document.getElementById('contactar');
+hablar.addEventListener('click', avisame);
+
+function avisame (){
+    alert('celialopez1503@gmail.com')
+}; 
+
+
+
+
